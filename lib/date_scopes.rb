1e8d01b84411e304223c2ddb6e1e1f7f8453cf90
Victor Sokolov <gzigzigzeo@gmail.com>
2010-12-07 20:00:27 +0300

    railtie fix

--- lib/date_scopes.rb
+++ lib/date_scopes.rb
@@ -1,3 +1,3 @@
 require 'date_scopes/arel'
-require 'date_scopes/active_record' if defined?(ActiveRecord::Base)
+require 'date_scopes/active_record' if defined?(ActiveRecord)
 require 'date_scopes/railtie' if defined?(Rails)
