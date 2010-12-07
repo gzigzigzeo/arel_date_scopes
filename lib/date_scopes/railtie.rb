03cbebe4a135c468ba13e5a2e49e4f8153902c07
Victor Sokolov <gzigzigzeo@gmail.com>
2010-12-07 20:01:26 +0300

    railtie fix

--- lib/date_scopes/railtie.rb
+++ lib/date_scopes/railtie.rb
@@ -2,7 +2,7 @@ module DateScopes
   class Railtie < Rails::Railtie
     initializer 'fmt_alias.insert_into_active_record' do
       ActiveSupport.on_load :active_record do
-        ActiveRecord::Base.send(:include, DateScopes::ActiveRecord)
+        ::ActiveRecord::Base.send(:include, DateScopes::ActiveRecord)
       end
     end
   end
